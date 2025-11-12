1dcb9f057784f0c963d2df909237a5c8ca1532e1
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-11-12 12:17:41 +0100

    Initial commit

---
+++ ARIA/ARIA/ARIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARIAApp.swift
+//  ARIA
+//
+//  Created by Sébastien DAGUIN on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ARIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
